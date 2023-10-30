5e963817c124997552e79083c1c523c7c702ff96
sandesh kshirsagar <sandeshkshirsagar505@gmail.com>
2023-10-30 16:28:28 +0530

    Add project files.

---
+++ FileIODemo/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace FileIODemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtid = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtSalary = new System.Windows.Forms.TextBox();
+            this.lblid = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblSalary = new System.Windows.Forms.Label();
+            this.btnWrite = new System.Windows.Forms.Button();
+            this.btnRaed = new System.Windows.Forms.Button();
+            this.btnCreateDirectory = new System.Windows.Forms.Button();
+            this.btnCreateFile = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnStreamRead = new System.Windows.Forms.Button();
+            this.btnStreamWrite = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtid
+            // 
+            this.txtid.Location = new System.Drawing.Point(118, 63);
+            this.txtid.Name = "txtid";
+            this.txtid.Size = new System.Drawing.Size(100, 20);
+            this.txtid.TabIndex = 0;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(118, 117);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // txtSalary
+            // 
+            this.txtSalary.Location = new System.Drawing.Point(118, 181);
+            this.txtSalary.Name = "txtSalary";
+            this.txtSalary.Size = new System.Drawing.Size(100, 20);
+            this.txtSalary.TabIndex = 2;
+            // 
+            // lblid
+            // 
+            this.lblid.AutoSize = true;
+            this.lblid.Location = new System.Drawing.Point(58, 70);
+            this.lblid.Name = "lblid";
+            this.lblid.Size = new System.Drawing.Size(15, 13);
+            this.lblid.TabIndex = 3;
+            this.lblid.Text = "id";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(58, 124);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(35, 13);
+            this.lblName.TabIndex = 4;
+            this.lblName.Text = "Name";
+            // 
+            // lblSalary
+            // 
+            this.lblSalary.AutoSize = true;
+            this.lblSalary.Location = new System.Drawing.Point(58, 188);
+            this.lblSalary.Name = "lblSalary";
+            this.lblSalary.Size = new System.Drawing.Size(36, 13);
+            this.lblSalary.TabIndex = 5;
+            this.lblSalary.Text = "Salary";
+            // 
+            // btnWrite
+            // 
+            this.btnWrite.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWrite.Location = new System.Drawing.Point(72, 258);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(77, 34);
+            this.btnWrite.TabIndex = 6;
+            this.btnWrite.Text = "Write";
+            this.btnWrite.UseVisualStyleBackColor = true;
+            this.btnWrite.Click += new System.EventHandler(this.btnWrite_Click);
+            // 
+            // btnRaed
+            // 
+            this.btnRaed.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRaed.Location = new System.Drawing.Point(171, 258);
+            this.btnRaed.Name = "btnRaed";
+            this.btnRaed.Size = new System.Drawing.Size(74, 34);
+            this.btnRaed.TabIndex = 7;
+            this.btnRaed.Text = "Read";
+            this.btnRaed.UseVisualStyleBackColor = true;
+            this.btnRaed.Click += new System.EventHandler(this.btnRaed_Click);
+            // 
+            // btnCreateDirectory
+            // 
+            this.btnCreateDirectory.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCreateDirectory.Location = new System.Drawing.Point(285, 60);
+            this.btnCreateDirectory.Name = "btnCreateDirectory";
+            this.btnCreateDirectory.Size = new System.Drawing.Size(127, 39);
+            this.btnCreateDirectory.TabIndex = 8;
+            this.btnCreateDirectory.Text = "Create Directory";
+            this.btnCreateDirectory.UseVisualStyleBackColor = true;
+            this.btnCreateDirectory.Click += new System.EventHandler(this.btnCreateDirectory_Click);
+            // 
+            // btnCreateFile
+            // 
+            this.btnCreateFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCreateFile.Location = new System.Drawing.Point(285, 117);
+            this.btnCreateFile.Name = "btnCreateFile";
+            this.btnCreateFile.Size = new System.Drawing.Size(127, 40);
+            this.btnCreateFile.TabIndex = 9;
+            this.btnCreateFile.Text = "Create File";
+            this.btnCreateFile.UseVisualStyleBackColor = true;
+            this.btnCreateFile.Click += new System.EventHandler(this.btnCreateFile_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(463, 60);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(271, 104);
+            this.richTextBox1.TabIndex = 10;
+            this.richTextBox1.Text = "";
+            // 
+            // btnStreamRead
+            // 
+            this.btnStreamRead.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStreamRead.Location = new System.Drawing.Point(607, 213);
+            this.btnStreamRead.Name = "btnStreamRead";
+            this.btnStreamRead.Size = new System.Drawing.Size(127, 40);
+            this.btnStreamRead.TabIndex = 11;
+            this.btnStreamRead.Text = "Stream Read";
+            this.btnStreamRead.UseVisualStyleBackColor = true;
+            this.btnStreamRead.Click += new System.EventHandler(this.btnStreamRead_Click);
+            // 
+            // btnStreamWrite
+            // 
+            this.btnStreamWrite.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStreamWrite.Location = new System.Drawing.Point(463, 213);
+            this.btnStreamWrite.Name = "btnStreamWrite";
+            this.btnStreamWrite.Size = new System.Drawing.Size(127, 40);
+            this.btnStreamWrite.TabIndex = 12;
+            this.btnStreamWrite.Text = "Stream Write";
+            this.btnStreamWrite.UseVisualStyleBackColor = true;
+            this.btnStreamWrite.Click += new System.EventHandler(this.btnStreamWrite_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 448);
+            this.Controls.Add(this.btnStreamWrite);
+            this.Controls.Add(this.btnStreamRead);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnCreateFile);
+            this.Controls.Add(this.btnCreateDirectory);
+            this.Controls.Add(this.btnRaed);
+            this.Controls.Add(this.btnWrite);
+            this.Controls.Add(this.lblSalary);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblid);
+            this.Controls.Add(this.txtSalary);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtid);
+            this.Name = "Form1";
+            this.Text = "File IO Demo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtid;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtSalary;
+        private System.Windows.Forms.Label lblid;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblSalary;
+        private System.Windows.Forms.Button btnWrite;
+        private System.Windows.Forms.Button btnRaed;
+        private System.Windows.Forms.Button btnCreateDirectory;
+        private System.Windows.Forms.Button btnCreateFile;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnStreamRead;
+        private System.Windows.Forms.Button btnStreamWrite;
+    }
+}
+
---
+++ FileIODemo/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace FileIODemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+     
+
+        private void btnCreateDirectory_Click(object sender, EventArgs e)
+        //{
+        //    try
+        //    {
+        //        string path = @"C:\TestFolder";
+
+
+        //        if (Directory.Exists(path))
+        //        {
+        //            MessageBox.Show("Directory alredy exists");
+        //        }
+        //        else
+        //        {
+        //            Directory.CreateDirectory(path);
+        //            MessageBox.Show("Directory Created");
+        //        }
+        //    }
+        //    catch(Exception ex)
+        //    {
+        //        MessageBox.Show(ex.Message);
+        //    }
+        //}
+
+        {
+            try
+            {
+                string path = @"C:\TestFolder";
+                DirectoryInfo info = new DirectoryInfo(path);
+
+
+                if (info.Exists)
+                {
+                    MessageBox.Show("Directory already exists...");
+                }
+                else
+                {
+                    info.Create();
+                    MessageBox.Show("Directory Created...");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnCreateFile_Click(object sender, EventArgs e)
+        {
+            //try 
+            //{
+            //    string path = @"C:\TestFolder\Employee.dat"; //.dat-->Data file(binary)
+
+            //    if(File.Exists(path))
+            //    {
+            //        MessageBox.Show("File alredy exists...");
+            //    }
+            //    else
+            //    {
+            //        File.Create(path);
+            //        MessageBox.Show("File Created");
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show(ex.Message);
+            //}
+
+            try
+            {
+                string path = @"C:\TestFolder\Employee.dat"; //.dat-->Data file(binary)
+                FileInfo file = new FileInfo(path);
+
+                if (file.Exists)
+                {
+                    MessageBox.Show("File already exists...");
+                }
+                else
+                {
+                    file.Create();
+                    MessageBox.Show("File Created...");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FileStream fs = new FileStream(@"C:\TestFolder\Employee.dat", FileMode.Create, FileAccess.Write);
+                BinaryWriter bw = new BinaryWriter(fs);
+                bw.Write(Convert.ToInt32(txtid.Text));
+                bw.Write(txtName.Text);
+                bw.Write(Convert.ToDouble(txtSalary.Text));
+                bw.Close();
+                fs.Close();
+                MessageBox.Show("Done");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btnRaed_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FileStream fs = new FileStream(@"C:\TestFolder\Employee.dat", FileMode.Open, FileAccess.Read);
+                BinaryReader br = new BinaryReader(fs);
+                txtid.Text = br.ReadInt32().ToString();
+                txtName.Text = br.ReadString();
+                txtSalary.Text = br.ReadDouble().ToString();
+                br.Close();
+                fs.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btnStreamWrite_Click(object sender, EventArgs e)
+        {
+            try 
+            {
+                FileStream fs = new FileStream(@"C:\TestFolder\data.txt", FileMode.Create, FileAccess.Write);
+                StreamWriter sw = new StreamWriter(fs);
+                sw.WriteLine(richTextBox1.Text);
+                sw.Close();
+                fs.Close();
+                MessageBox.Show("Done");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnStreamRead_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FileStream fs = new FileStream(@"C:\TestFolder\data.txt", FileMode.Open, FileAccess.Read);
+                StreamReader sr = new StreamReader(fs);
+                richTextBox1.Text = sr.ReadToEnd();
+                sr.Close();
+                fs.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
